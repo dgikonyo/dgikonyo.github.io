398b8e357a436c605f4f3ada7864e5c7b125e136
dgikonyo <denniskimani05@gmail.com>
2021-11-12 21:47:24 +0300

    Admin side done

--- disaster/urls.py
+++ disaster/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('',views.index,name='index'),#views.index is a function called index() in the views.py file
+    path('alerts/',views.DisasterListView.as_view(),name='alerts'),
+    path('alerts/<int:pk',views.DisasterDetailView.as_view(),name='alert_detail'),
 
 ]
+
--- disaster/views.py
+++ disaster/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+from .models import User,DisasterReport,Disaster
+
+"""Index is a function used in rendering the details to be included
+in the index.html file"""
+def index(request):
+    """view function for home page of site"""
+
+    #generate a list of all disasters
+    floodEvent=DisasterReport
+    floodLocations=DisasterReport.objects.filter(validated=True).count()
+    num_floods=DisasterReport.objects.count()
+    #will give a count of all possible number of floods expected
+
+    context={
+        'floodEvent':floodEvent,
+        'floodLocations':floodLocations,
+        'num_floods':num_floods,
+    }
+    #render the html template index.html with data in the context variable
+    return render(request, 'index.html',context=context)
+
+"""The class is used to generate a list of all alerts that have been validated
+"""
+class DisasterListView(generic.ListView):
+    model=DisasterReport
+    context_object_name='alerts_list'#is the name of the list
+    template_name='disaster/index.html'#is the target html file that will have the alerts
+
+    def get_queryset(self):
+        #list upto five validated alertss
+        return DisasterReport.objects.filter(validated=True)[:5]
+
+class DisasterDetailView(generic.DetailView):
+    
