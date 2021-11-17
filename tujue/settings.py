2d3a46c96b763dc80f12b5136e66133e3c415735
dgikonyo <denniskimani05@gmail.com>
2021-11-18 02:00:11 +0300

    Automatic configuration with django-heroku

--- tujue/settings.py
+++ tujue/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +142,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
+
+#meant for hosting on heroku
+django_heroku.settings(locals())
